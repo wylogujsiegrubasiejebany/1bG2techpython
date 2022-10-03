d4fe6919d8cff4fca60245fba8dda82da027619c
1b7aj <114579333+RizierBirch@users.noreply.github.com>
2022-10-03 11:19:40 +0000

    zrobiono zadania 1-4 z karty pracy 1

---
+++ KartaPracy1.py
@@ -0,0 +1,24 @@
+# zad 1
+
+# # a=int(imput())
+# # b=int(inout())
+# # print(a**2 + b**2)
+
+# zad 2
+
+# a = int(input())
+# b = int(imput())
+# print((a+b)**2)
+
+# zad 3
+
+# a = int(input())
+# b = int(input())
+# print ((a+b)**3)
+
+# zad4
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# print (a*b*c)
