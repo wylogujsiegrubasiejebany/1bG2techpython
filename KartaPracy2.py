4121ccf7457d8c0b73375e99f87922b6a4c570c0
1b7aj <114579333+RizierBirch@users.noreply.github.com>
2022-10-05 11:19:44 +0000

    4 zadania

---
+++ KartaPracy2.py
@@ -0,0 +1,25 @@
+# zad 1
+# a = int (input("podaj liczbe"))
+# if a % 3 == 0:
+#     print("TAK")
+# else: 
+#     print("NIE")
+
+# zad 2 
+# a = int(input("podaj liczbe: "))
+# if a>=100 and a<1000 and a%17==0:
+#       print ("tak")
+# else:
+#     print("nie")
+
+# zad 3
+# wiek = int(imput("ile lat masz?"))
+# if wiek>= 18:
+#         print("tak")
+# else:
+#       print("nie")
+
+# zad 4
+limit = 20 
+waga = int(input("ile wazy gruz"))
+if waga<=
