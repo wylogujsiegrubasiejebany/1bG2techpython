75514c5c831fdbc0424a805367a6d39b7ee68b3c
1b7aj <114579333+RizierBirch@users.noreply.github.com>
2022-10-05 10:57:28 +0000

    ukonczono karte pracu 1

--- KartaPracy1.py
+++ KartaPracy1.py
@@ -22,4 +22,21 @@
 # b = int(input())
 # c = int(input())
 # print (a*b*c) 
-,
+
+# zad 5
+
+# a = int(input())
+# b = int(input())
+# print(2∗(a+b)/5)
+
+# zad 6
+
+# btutto = int(input())
+
+# print (brutto/1.23)
+
+# zad 7 
+
+# a=int(imput())
+# b=int(imput())
+# print (a%b)
