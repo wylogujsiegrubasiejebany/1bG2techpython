0a1bbaa3953feb4fb0c6f8c384ead97667476a55
1b7aj <114579333+RizierBirch@users.noreply.github.com>
2022-11-01 19:36:21 +0000

    j

--- KartaPracy3b.py
+++ KartaPracy3b.py
@@ -1,10 +1,65 @@
-# zad1 
+# zad 1
+# for i in range(1,30,1): print(i,end=" ")
 
-# for i in range(1,30): print(i,end=" ")
+# zad 2
+# for i in range(1,20,2): print(i**2)
 
-# zad 2 
+# zad 3
+# for i in range(1000, 10000): 
+#     if i%379 == 0:
+#         print(i)
 
-# for i in range(1,10,2):
-#   print(i**2, end=" ")
 
-# zad 3 
+# zad 4
+# for i in range(100, 1000):
+#     if i % 5 and 6 or 11 == 0:
+#         print(i)
+
+# #zad 5
+# n = int(input("ile liczb chcesz wpisać? "))
+# suma=0
+# for  i in range(n):
+#   k = int(input())
+#   suma+=k
+# print(suma)
+
+#zad 6
+# a = int(input())
+# groot=0
+# for i in range(2,a+1,2):
+#  groot+=i
+# print(a)
+
+# #zad 7
+# m = int(input())
+# a=0
+# for i in range(11, m+1, 2):
+#   a+=i
+# print(a)
+
+#Zad 8
+# W = int(input())
+# L = int(input())
+# c = 0
+# suma = W
+# for i in range(0, L * 12):
+#   c = suma * 0.06 * (1/12)
+#   suma += c
+# print(round(suma,2))
+
+
+#zad 9
+# n = int(input())
+# suma=0
+# for i in range(21,n+1,100):
+#  suma+=i
+# print(suma)
+
+
+#Zad 10
+# import math
+# for i in range(1, 1000):
+#  if i % 10 == math.sqrt(i):
+#    print(i)
+#  elif i % 100 == math.sqrt(i):
+#    print(i)
